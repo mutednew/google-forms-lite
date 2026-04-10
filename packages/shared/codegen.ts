f118ebeb8a0e778ff0cc61fa4035e75e2f186b5f
mutednew <artehaguk@gmail.com>
2026-04-10 23:48:21 +0300

    feat(shared): add codegen config and generated types

--- packages/shared/codegen.ts
+++ packages/shared/codegen.ts
@@ -1,8 +1,9 @@
 import type { CodegenConfig } from "@graphql-codegen/cli"
 
 const config: CodegenConfig = {
-    schema: "../server/src/schema.graphql",
-    documents: "../client/src/**/*.graphql",
+    schema: "../../apps/server/src/schema.graphql",
+    documents: "../../apps/client/src/**/*.graphql",
+    ignoreNoDocuments: true,
     generates: {
         "./src/generated.ts": {
             plugins: [
